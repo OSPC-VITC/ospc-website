5fcd174470d6e6b447bfdad23004d5ed5ffb6dc5
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2025-01-01 18:09:10 +0530

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -8,95 +8,85 @@ import Terminal from "./Terminal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import ParticlesComponent from "@/components/Particles";
+import { Spotlight } from "@/components/ui/spotlight";
+import { FaLocationArrow } from "react-icons/fa";
+import { TextGenerateEffect } from "@/components/ui/TextGenerateEffect";
+import MagicButton from "@/components/MagicButton";
 
-const PARALLAX_FACTOR = 0.5;
-const BACKGROUND_SHIFT = 0.1;
-const INITIAL_BACKGROUND_POSITION = 20;
 
-const IMAGES = {
-  mainBg: { 
-    src: "/main-bg.webp",
-    alt: "Abstract background pattern" 
-  }
-};
 
 const fadeInUp = {
   hidden: { y: 20, opacity: 0 },
   visible: { y: 0, opacity: 1 }
 };
 
-interface HeroSectionProps {
-  scrollY: number;
-}
+const Hero: React.FC = () => {
+  return (
+    <div className="pb-20 pt-36">
+      
+      {/**
+       * UI: Spotlights
+       * Link: https://ui.aceternity.com/components/spotlight
+       */}
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
 
-const HeroSection: React.FC<HeroSectionProps> = ({ scrollY }) => (
-  <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 0.8 }}
-    className="relative min-h-screen flex flex-col lg:flex-row items-center"
-  >
-    <div
-      className="absolute inset-0 bg-cover bg-center transition-transform duration-300"
-      style={{
-        backgroundImage: `url(${IMAGES.mainBg.src})`,
-        backgroundPosition: `center ${INITIAL_BACKGROUND_POSITION - scrollY * BACKGROUND_SHIFT}%`,
-        transform: `translateY(${scrollY * PARALLAX_FACTOR}px)`,
-      }}
-    >
+      {/**
+       * UI: grid
+       */}
       <div
-        className="absolute inset-0 bg-black bg-opacity-55"
-        style={{
-          backdropFilter: "blur(4px)",
-        }}
-      />
-    </div>
-
-    <div className="flex flex-col lg:flex-row items-center w-full relative z-10 px-6 py-10 lg:py-0">
-      <motion.div
-        variants={fadeInUp}
-        initial="hidden"
-        animate="visible"
-        transition={{ delay: 0.3 }}
-        className="w-full lg:w-1/2 lg:pl-20 flex flex-col gap-5 mb-10 lg:mb-0"
+        className="h-screen w-full 
+        absolute top-0 left-0 flex items-center justify-center"
       >
-        <h1 className="text-3xl md:text-[45px] text-white font-semibold leading-tight">
-          OSPC VIT, Chennai was created to spread awareness of{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 animate-gradient">
-            Free and Open Source Software (FOSS)
-          </span>{" "}
-          and{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600 animate-gradient">
-            Git and GitHub
-          </span>
-          <FontAwesomeIcon icon={faGithub} className="text-blue-500 ml-2" />
-        </h1>
-      </motion.div>
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
 
-      <div className="w-full lg:w-1/2 flex justify-center px-4">
-        <Terminal />
+      <div className="flex justify-center relative my-20 z-10">
+        <ParticlesComponent />
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-lg text-center text-white-200 max-w-80">
+            OSPC
+          </p>
+
+          {/**
+           * Link: https://ui.aceternity.com/components/text-generate-effect
+           */}
+          <TextGenerateEffect
+            words="Open Source Programming Club VIT Chennai"
+            className=" text-white-100 text-center text-[40px] md:text-5xl lg:text-6xl "
+          />
+
+          <p className=" text-white-200 text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          Promoting awareness and adoption of Free and Open Source Software (FOSS) and empowering students with Git and GitHub expertise at OSPC VIT, Chennai.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="About OSPC"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
       </div>
     </div>
-  </motion.div>
-);
+  );
+};
 
-const DecorativeImages = () => (
-  <>
-    <motion.div
-      className="absolute top-0 right-0 z-30"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 4.0 }}
-    ></motion.div>
 
-    <motion.div
-      initial={{ rotate: -10, opacity: 0 }}
-      animate={{ rotate: 0, opacity: 1 }}
-      transition={{ delay: 0.6, duration: 4.0}}
-      className="absolute top-0 left-0 z-10"
-    ></motion.div>
-  </>
-);
+
 
 const AboutSection = () => (
   <motion.section
@@ -109,7 +99,7 @@ const AboutSection = () => (
   >
     <div className="max-w-3xl mx-auto px-6">
       <ParticlesComponent id="particles-background" />
-      <h2 className="text-4xl font-bold mb-6 bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent">
+      <h2 className=" text-white-100 text-4xl font-bold mb-6 bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent">
         About OPSC
       </h2>
       <p className="mb-4 text-lg leading-relaxed">
@@ -146,7 +136,7 @@ const EntryAnimation = ({ onComplete }: { onComplete: () => void }) => (
     onAnimationComplete={onComplete}
   >
     <div className="text-center">
-      <h1 className="text-4xl md:text-6xl text-white font-bold mb-4">
+      <h1 className="text-4xl md:text-6xl text-white-200 font-bold mb-4">
         Welcome to OPSC
       </h1>
       <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-300 mb-2">
@@ -179,8 +169,9 @@ export default function Home() {
 
   return (
     <main className="relative overflow-hidden bg-gradient-to-b from-dark-800 to-purple-400">
-      <HeroSection scrollY={scrollY} />
-      <DecorativeImages />
+      <Spotlight />
+      <Hero />
+      <Terminal />
       <AboutSection />
     </main>
   );
