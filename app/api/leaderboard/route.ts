22a51403a7954fb4ff5b8af8c95057d9c0974d63
OSPC_VITC <opensourceprogrammingclub.vitc@gmail.com>
2025-06-28 19:45:56 +0530

    points table 25-26

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -2,7 +2,7 @@ import { supabase } from "@/utils/supabaseClient";
 
 export async function GET() {
     const { data, error } = await supabase
-        .from("Points Table 24-25")
+        .from("Points Table 25-26")
         .select("Name, OSPC_ID, REG_NO, Points");
 
     return Response.json({
